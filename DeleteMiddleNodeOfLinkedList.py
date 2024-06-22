f9a8cf107cfbb3059c169e853cb86aa61e4031f0
Shivangee Kulkarni <43023907+kshivangee@users.noreply.github.com>
2024-06-22 15:48:57 -0500

    Update DeleteMiddleNodeOfLinkedList.py
    Added another approach using the Linked list traversal by keeping count of the nodes and then deleting the middle node...

--- DeleteMiddleNodeOfLinkedList.py
+++ DeleteMiddleNodeOfLinkedList.py
@@ -9,13 +9,33 @@ class Solution(object):
         :type head: Optional[ListNode]
         :rtype: Optional[ListNode]
         """
-        numlist=[]
-        while(head):
-            numlist.append(head.val)
-            head=head.next
-        del numlist[len(numlist)//2]
-        newhead = currentnode = ListNode(0)
-        for i in numlist:
-            currentnode.next = ListNode(i)
-            currentnode = currentnode.next
-        return newhead.next
+        # Approach 1: Traversing the linked list by counting the elements in a list and then deleting middle element 
+        count = 0
+        newhead = head
+        while(newhead):
+            count+=1
+            newhead=newhead.next
+        middle = count//2
+        if(middle == 0):
+            return None
+        index = 0
+        newhead = head
+        while(newhead):
+            if(index == middle-1):
+                newhead.next = newhead.next.next
+            newhead = newhead.next
+            index+=1
+        return head 
+
+
+        # Approach 2: Traversing the linked list by appending the elements in a list and then deleting middle element from the list and converting list to linkedlist
+        # numlist=[]
+        # while(head):
+        #     numlist.append(head.val)
+        #     head=head.next
+        # del numlist[len(numlist)//2]
+        # newhead = currentnode = ListNode(0)
+        # for i in numlist:
+        #     currentnode.next = ListNode(i)
+        #     currentnode = currentnode.next
+        # return newhead.next
