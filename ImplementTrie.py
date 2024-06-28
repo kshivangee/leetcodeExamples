850533e42db329381376cd3097238bcc1555dd4b
Shivangee Kulkarni <43023907+kshivangee@users.noreply.github.com>
2024-06-27 23:58:03 -0500

    Update ImplementTrie.py
    Added approach #2: Using Trie datastructure...

--- ImplementTrie.py
+++ ImplementTrie.py
@@ -1,42 +1,71 @@
-class Trie(object):
-
+class TrieNode:
     def __init__(self):
-        self.list1=[]
+        self.childnode = [None]*26
+        self.wordend=False
+
+class Trie(object):
+    def __init__(self):
+        # self.list1=[]
+        #Approach 2: Using Trie data structure
+        self.root = TrieNode()
 
     def insert(self, word):
         """
         :type word: str
         :rtype: None
         """
-        self.list1.append(word)
+        # self.list1.append(word)
+        
+        current = self.root
+        for i in word:
+            index = ord(i)-ord('a')
+            if(not current.childnode[index]):
+                current.childnode[index]= TrieNode()
+            current = current.childnode[index]
+        current.wordend=True
         
     def search(self, word):
         """
         :type word: str
         :rtype: bool
         """
-        try:
-            if(word in self.list1):
-                return True
-            else:
+        # try:
+        #     if(word in self.list1):
+        #         return True
+        #     else:
+        #         return False
+        # except:
+        #     pass
+        
+        current = self.root
+        for i in word:
+            index = ord(i)-ord('a')
+            if(current.childnode[index] is None):
                 return False
-        except:
-            pass
+            current = current.childnode[index]
+        return current.wordend
     
-
     def startsWith(self, prefix):
         """
         :type prefix: str
         :rtype: bool
         """
-        try:
-            for i in self.list1:
-                if(i.startswith(prefix)):
-                    return True
-            else:
+        # try:
+        #     for i in self.list1:
+        #         if(i.startswith(prefix)):
+        #             return True
+        #     else:
+        #         return False
+        # except:
+        #     pass
+        
+        current = self.root
+        for i in prefix:
+            index=ord(i)-ord('a')
+            if(current.childnode[index] is None):
                 return False
-        except:
-            pass
+            current = current.childnode[index]
+        return True
         
 
 # Your Trie object will be instantiated and called as such:
