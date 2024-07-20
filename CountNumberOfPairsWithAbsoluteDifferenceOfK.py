38dbf7276f9b0c05ed29ba1a7db4d714d033341f
Shivangee Kulkarni <43023907+kshivangee@users.noreply.github.com>
2024-07-19 20:44:09 -0500

    Count Number of Pairs With Absolute Difference K

---
+++ CountNumberOfPairsWithAbsoluteDifferenceOfK.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def countKDifference(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: int
+        """
+        count=0
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if(abs(nums[i]-nums[j])==k):
+                    count+=1
+        return count
+
+        
