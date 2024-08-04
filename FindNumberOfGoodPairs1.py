6fa98daa3a2d6752b2e75fa7d193b33d6b9e2116
Shivangee Kulkarni <43023907+kshivangee@users.noreply.github.com>
2024-08-04 00:04:09 -0500

    Find Number Of Good Pairs1

---
+++ FindNumberOfGoodPairs1.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def numberOfPairs(self, nums1, nums2, k):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :type k: int
+        :rtype: int
+        """
+        result = 0
+        for i in range(len(nums1)):
+            for j in range(len(nums2)):
+                if(nums1[i]%(nums2[j]*k)==0):
+                    result+=1
+        return result
+
+        
