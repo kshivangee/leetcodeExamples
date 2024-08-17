2b409300c7c48049166772ac183d477966573d39
Shivangee Kulkarni <43023907+kshivangee@users.noreply.github.com>
2024-08-16 21:59:18 -0500

    Counting Words With a Given Prefix

---
+++ CountingWordsWithaGivenPrefix.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def prefixCount(self, words, pref):
+        """
+        :type words: List[str]
+        :type pref: str
+        :rtype: int
+        """
+        count=0
+        for i in words:
+            if(i.startswith(pref)):
+                count+=1
+        return count
