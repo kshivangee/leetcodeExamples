401f6fa0c7080c68dd4dead30f6d990d8a95e548
Shivangee Kulkarni <43023907+kshivangee@users.noreply.github.com>
2024-09-09 20:13:41 -0500

    Update InsertGreatestCommonDivisorsinLinkedList.py
    Added another way of solving the problem

--- InsertGreatestCommonDivisorsinLinkedList.py
+++ InsertGreatestCommonDivisorsinLinkedList.py
@@ -9,6 +9,35 @@ class Solution(object):
         :type head: Optional[ListNode]
         :rtype: Optional[ListNode]
         """
+        #Approach 1: Using List
+        list1=[]
+        newhead = head
+        while(head):
+            list1.append(head.val)
+            head = head.next
+        # print list1
+        if(len(list1)==1):
+            return newhead
+        resultlist = []
+        for i in range(len(list1)-1):
+            num1= list1[i]
+            num2 = list1[i+1]
+            result = min(num1,num2)
+            while(result):
+                if(num1%result == 0 and num2%result==0):
+                    break
+                result-=1
+            resultlist.append(num1)
+            resultlist.append(result)
+            if(i==len(list1)-2):
+                resultlist.append(num2)
+        dummy = newhead = ListNode(0)
+        for i in resultlist:
+            dummy.next = ListNode(i)
+            dummy = dummy.next
+        return newhead.next
+        
+        #Approach 2: Using Linked list
         if(head is None):
             return None
         if(head.next is None):
@@ -30,6 +59,3 @@ class Solution(object):
             current = current.next
             newNode.next = newhead
         return head
-            
-
-        
