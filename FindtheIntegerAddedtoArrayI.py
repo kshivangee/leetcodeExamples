b47dd57b13239f7fb558cf7afb10decb408f0453
Shivangee Kulkarni <43023907+kshivangee@users.noreply.github.com>
2024-09-10 19:34:27 -0500

    Find the Integer Added to Array I

---
+++ FindtheIntegerAddedtoArrayI.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def addedInteger(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: int
+        """
+        nums1.sort()
+        nums2.sort()
+        return min(nums2)-min(nums1)
