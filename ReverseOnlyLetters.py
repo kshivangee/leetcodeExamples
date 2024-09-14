3f78ccf0d1a6cbf4c8a2f7f45945b7bcf1fadb3e
Shivangee Kulkarni <43023907+kshivangee@users.noreply.github.com>
2024-09-13 23:27:28 -0500

    Reverse Only Letters

---
+++ ReverseOnlyLetters.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def reverseOnlyLetters(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        start,end = 0,len(s)-1
+        s = list(s)
+        while (start<end):
+            if s[start].isalpha() and s[end].isalpha():
+                s[start],s[end] = s[end],s[start]
+                start += 1
+                end -= 1
+            elif s[start].isalpha() and s[end].isalpha() == False:
+                end -= 1
+            elif s[start].isalpha() == False and s[end].isalpha():
+                start += 1
+            else:
+                start += 1
+                end -= 1
+        return "".join(s)
+       
